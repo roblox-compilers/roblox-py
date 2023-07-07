26a589ae0b28eed4742e1d8f71f066a0b938b792
procoder <72946059+AsynchronousAI@users.noreply.github.com>
2023-07-07 16:09:57 -0500

    Create header.lua

---
+++ header.lua
@@ -0,0 +1,6 @@
+--// Compiled using Roblox.py \\--
+		
+		
+------------------------------------ BUILT IN -------------------------------
+local stringmeta, list, dict, staticmethod, class, range, __name__, len, abs, str, int, sum, max, min, reversed, split, round, all, any, ord, char, callable, zip, float, format, hex, id, map = unpack(require(game.ReplicatedStorage["Roblox.py"])(script))
+-----------------------------------------------------------------------------
