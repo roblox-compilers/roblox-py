06b045066dd57e618d0a00fdf3667f3e34cb6fba
procoder <72946059+AsynchronousAI@users.noreply.github.com>
2023-07-06 20:23:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,75 @@
 from flask import Flask, request
 from pythonlua.translator import Translator
+from pyflakes import api
+import re
 
-import sys
-import pylint
+class Reporter:
+    """
+    Formats the results of pyflakes checks to users.
+    """
+    def __init__(self):
+        self.diagnostics = []
 
+    def unexpectedError(self, filename, msg):
+        """
+        An unexpected error occurred trying to process C{filename}.
 
+        @param filename: The path to a file that we could not process.
+        @ptype filename: C{unicode}
+        @param msg: A message explaining the problem.
+        @ptype msg: C{unicode}
+        """
+        self.diagnostics.append(f"{filename}: {msg}\n")
 
-from io import StringIO
+    def syntaxError(self, filename, msg, lineno, offset, text):
+        """
+        There was a syntax error in C{filename}.
 
-from pylint.lint import Run
-from pylint.reporters.text import TextReporter
+        @param filename: The path to the file with the syntax error.
+        @ptype filename: C{unicode}
+        @param msg: An explanation of the syntax error.
+        @ptype msg: C{unicode}
+        @param lineno: The line number where the syntax error occurred.
+        @ptype lineno: C{int}
+        @param offset: The column on which the syntax error occurred, or None.
+        @ptype offset: C{int}
+        @param text: The source code containing the syntax error.
+        @ptype text: C{unicode}
+        """
+        if text is None:
+            line = None
+        else:
+            line = text.splitlines()[-1]
 
+        # lineno might be None if the error was during tokenization
+        # lineno might be 0 if the error came from stdin
+        lineno = max(lineno or 0, 1)
+
+        if offset is not None:
+            # some versions of python emit an offset of -1 for certain encoding errors
+            offset = max(offset, 1)
+            self.diagnostics.append('%s:%d:%d: %s\n' %
+                               (filename, lineno, offset, msg))
+        else:
+            self.diagnostics.append('%s:%d: %s\n' % (filename, lineno, msg))
+
+        if line is not None:
+            self.diagnostics.append(line)
+            self.diagnostics.append('\n')
+            if offset is not None:
+                self.diagnostics.append(re.sub(r'\S', ' ', line[:offset - 1]) +
+                                   "^\n")
+
+    def flake(self, message):
+        """
+        pyflakes found something wrong with the code.
+
+        @param: A L{pyflakes.messages.Message}.
+        """
+        self.diagnostics.append(str(message))
+        self.diagnostics.append('\n')
+      
 app = Flask(__name__)
-pylint_output = StringIO()
-reporter = TextReporter(pylint_output)
-
-def check(source):
-  with open("debug.py", 'w') as file:
-    file.write(source)
-    Run(["debug.py"], reporter=reporter, exit=False)
-    return (pylint_output.getvalue())
 
 @app.route('/', methods=["GET", "POST"]) 
 def base_page():
@@ -33,12 +83,13 @@ def base_page():
 
   return lua_code
 
-@app.route('/debug', methods=["GET", "POST"]) 
+@app.route('/err', methods=["GET", "POST"]) 
 def debug():
   code = (request.data).decode()
-
-  return check(code)
-
+  rep = Reporter()
+  num = str(api.check(code, "%s", rep))
+  print(num)
+  return rep.diagnostics
 app.run(
  host='0.0.0.0', 
  port=5555 
