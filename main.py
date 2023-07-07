538dd97edd93251ffd6b1704a4bc79436063031c
procoder <72946059+AsynchronousAI@users.noreply.github.com>
2023-07-06 20:40:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def base_page():
 def debug():
   code = (request.data).decode()
   rep = Reporter()
-  num = str(api.check(code, "_INTERNALSCRIPTNAME_?!", rep))
+  num = str(api.check(code, "%s", rep))
   print(num)
   return rep.diagnostics
 app.run(
