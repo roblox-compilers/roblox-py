a200e9c767765b4cf16b7169188d42cb52643325
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-07-08 14:47:31 -0500

    py3.13 dev implemented

--- test/dict.lua
+++ test/dict.lua
@@ -2,7 +2,7 @@
 		
 		
 ------------------------------------ BUILT IN -------------------------------
-local stringmeta, list, dict, staticmethod, class, range, __name__, len, abs, str, int, sum, max, min, reversed, split, round, all, any, ord, char, callable, zip, float, format, hex, id, map, bool, divmod, slice, operator_in, asynchronousfunction, match = unpack(require(game.ReplicatedStorage["Roblox.py"])(script))
+local stringmeta, list, dict, staticmethod, class, range, __name__, len, abs, str, int, sum, max, min, reversed, split, round, all, any, ord, char, callable, zip, float, format, hex, id, map, bool, divmod, slice, operator_in, asynchronousfunction, match, anext, ascii, dir, getattr, globals, hasattr, input, isinstance, issubclass, iter, locals, oct, open, ord, pow, eval, exec, filter, frozenset, aiter, bin, complex, delattr, enumerate, breakpoint, bytearray, bytes, compile, help, memoryview, repr, sorted, vars, __import__  = unpack(require(game.ReplicatedStorage["Roblox.py"])(script))
 -----------------------------------------------------------------------------
 local newdict = dict {}
 local newdict[stringmeta "one"] = 1
