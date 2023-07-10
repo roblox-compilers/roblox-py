19d192b93c3bfa17191c868c33e416819a0a2054
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-07-10 14:43:58 -0500

    Updated C

--- src/cnodevisitor.py
+++ src/cnodevisitor.py
@@ -1,58 +1,58 @@
 import clang.cindex
+from . import colortext
+
+# TODO:
+## Add depth/indentation to the code
+## Add all possible node types
 
 class NodeVisitor:
     def __init__(self):
         self.lua_code = ""
 
-    def visit_translation_unit(self, node):
+    def TRANSLATION_UNIT(self, node, depth=0):
         # Visit child nodes of the translation unit
         for child in node.get_children():
             self.visit_node(child)
 
-    def visit_function_decl(self, node):
+    def FUNCTION_DECL(self, node, depth=0):
         # Process function declarations
         function_name = node.spelling
         self.lua_code += f"function {function_name}()\n"
-        self.visit_compound_stmt(node.get_children())  # Visit the body of the function
-        self.lua_code += "end\n\n"
+        self.visit_compound_stmt(node.get_children())
 
-    def visit_var_decl(self, node):
+    def VAR_DECL(self, node, depth=0):
         # Process variable declarations
         variable_name = node.spelling
         self.lua_code += f"local {variable_name}\n"
 
-    def visit_compound_stmt(self, node):
+    def COMPOUND_STMT(self, node, depth=0):
         # Process compound statements (e.g., function bodies, loops, etc.)
-        self.lua_code += "{\n"
-        
-        #for child in node.children:
-        #    self.visit_node(child)
-        self.lua_code += "}\n"
+        self.lua_code += "\n"
+        if type(node) == list:
+            for child in node:
+                self.visit_node(child)
+        else:
+            for child in node.get_children():
+                self.visit_node(child)
+        self.lua_code += "end\n"
 
-    def visit_call_expr(self, node):
+    def CALL_EXPR(self, node, depth=0):
         # Process function calls
         function_name = node.spelling
         self.lua_code += f"{function_name}()\n"
 
-    def visit_decl_ref_expr(self, node):
+    def DECL_REF_EXPR(self, node, depth=0):
         # Process variable references
         variable_name = node.spelling
         self.lua_code += f"{variable_name}"
 
-    def visit_node(self, node):
+    
+    def visit_node(self, node, depth=0):
         # Dispatch visitation based on node kind
-        if node.kind == clang.cindex.CursorKind.TRANSLATION_UNIT:
-            self.visit_translation_unit(node)
-        elif node.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-            self.visit_function_decl(node)
-        elif node.kind == clang.cindex.CursorKind.VAR_DECL:
-            self.visit_var_decl(node)
-        elif node.kind == clang.cindex.CursorKind.COMPOUND_STMT:
-            self.visit_compound_stmt(node)
-        elif node.kind == clang.cindex.CursorKind.CALL_EXPR:
-            self.visit_call_expr(node)
-        elif node.kind == clang.cindex.CursorKind.DECL_REF_EXPR:
-            self.visit_decl_ref_expr(node)
+        if self[node.kind] is not None:
+            self[node.kind](node, depth+1)
+        else:
+            print(colortext.yellow(f"Warning: No visitor method for {node.kind}"))
 
         # Recursively visit child nodes
         for child in node.get_children():
