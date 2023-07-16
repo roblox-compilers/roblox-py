4215c92cc588f20259dfd2f7f724a3c8ceda95f7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-07-16 00:12:33 -0500

    Bug detection

--- src/luainit.py
+++ src/luainit.py
@@ -4,9 +4,11 @@ import sys
 
 initcode = ""
 
-with open("src/luainitlua.lua", "r") as f:
-    initcode = f.read()
-
+try:
+	with open("src/luainitlua.lua", "r") as f:
+		initcode = f.read()
+except FileNotFoundError:
+    print("roblox-pyc: Due to a bug, lib will not work, please report this issue to the github repo, discord server, or the devforum post\nthanks!")
 allfunctions = "stringmeta, list, dict, python, staticmethod, classsmethod, class, range, __name__, len, abs, str, int, sum, max, min, reversed, split, round, all, any, ord, char, callable, zip, float, format, hex, id, map, bool, divmod, slice, operator_in, asynchronousfunction, match, anext, ascii, dir, getattr, globals, hasattr, input, isinstance, issubclass, iter, locals, oct, open, ord, pow, eval, exec, filter, frozenset, aiter, bin, complex, delattr, enumerate, breakpoint, bytearray, bytes, compile, help, memoryview, repr, sorted, vars, __import_, formatmod"
 lunarfunctions = "type, table"
 allfunctions = allfunctions.split(", ")
