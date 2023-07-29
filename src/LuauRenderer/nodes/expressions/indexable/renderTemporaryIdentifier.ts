b14e9a453209e42dd901a2992a1abb2d1b06d19e
aqzp <72946059+AsynchronousAI@users.noreply.github.com>
2023-07-29 11:21:25 -0500

    I fucked up doing this

--- src/LuauRenderer/nodes/expressions/indexable/renderTemporaryIdentifier.ts
+++
@@ -1,9 +0,0 @@
-import luau from "LuauAST";
-import { assert } from "LuauAST/util/assert";
-import { RenderState } from "LuauRenderer";
-
-export function renderTemporaryIdentifier(state: RenderState, node: luau.TemporaryIdentifier) {
-	const name = state.getTempName(node);
-	assert(luau.isValidIdentifier(name), `Invalid Temporary Identifier: "${name}"`);
-	return name;
-}
